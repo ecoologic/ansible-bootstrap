a0d5328837ed36a92c28d52a94787017eb542a15
Erik Ecoologic <erikecoologic@gmail.com>
2021-08-01 18:22:23 +1000

    tasks/last-run

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 # "Installation completed successfully"
 ansible-playbook local.yaml
 ```
+
+## Test stuff with Docker
+
+```bash
+d run -v $(pwd):/home/user -it --entrypoint /bin/bash ubuntu
+apt update && apt install sudo
+./scripts/install-ansible-ubuntu.sh # 4, 2
+```
--- scripts/install-ansible-ubuntu.sh
+++ scripts/install-ansible-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update  && \
-sudo apt install software-properties-common && \
-sudo apt-add-repository --yes --update ppa:ansible/ansible && \
-sudo apt install ansible && \
+sudo apt install -y software-properties-common && \
+sudo apt-add-repository -y --update ppa:ansible/ansible && \
+sudo apt install -y ansible && \
 echo "Installation completed successfully"
